1632e1d3da977f3456b403c738d43ca1c7fabcb3
FlandreDaisuki <vbnm123c@gmail.com>
2021-07-31 14:52:00 +0800

    Make it to single file script

--- index.js
+++ index.js
@@ -2,15 +2,56 @@ import {
   isChildFrame,
   setupChildFrameMessageListener,
   setupParentFrameMessageListener,
+  onParentQuestion, askToParentFrame,
+  onChildQuestion, askToChildFrame,
 } from './message.js';
-
-if (isChildFrame()) {
-  setupChildFrameMessageListener();
-} else {
-  setupParentFrameMessageListener();
-}
 
 export const TOPIC = Object.freeze({
   COUNTER_TIMES: 'COUNTER_TIMES',
   INPUT_VALUE: 'INPUT_VALUE',
 });
+
+const setupChildFrameDOM = () => {
+  const inputEl = document.getElementById('input');
+  const answerEl = document.getElementById('answer');
+
+  document.getElementById('ask-btn').onclick = async() => {
+    const answer = await askToParentFrame(TOPIC.COUNTER_TIMES);
+    answerEl.textContent = answer;
+  };
+
+  onParentQuestion((msg) => {
+    if (msg.topic === TOPIC.INPUT_VALUE) {
+      return inputEl.value;
+    }
+  });
+};
+
+const setupParentFrameDOM = () => {
+  const outputEl = document.getElementById('output');
+  const answerEl = document.getElementById('answer');
+  const childEl = document.getElementById('child');
+
+  document.getElementById('inc-btn').onclick = () => {
+    outputEl.textContent = parseInt(outputEl.textContent) + 1;
+  };
+
+  document.getElementById('ask-btn').onclick = async() => {
+    const answer = await askToChildFrame(childEl, TOPIC.INPUT_VALUE);
+    answerEl.textContent = answer;
+  };
+
+  onChildQuestion((msg) => {
+    if (msg.topic === TOPIC.COUNTER_TIMES) {
+      return parseInt(outputEl.textContent);
+    }
+  });
+};
+
+if (isChildFrame()) {
+  setupChildFrameMessageListener();
+  setupChildFrameDOM();
+} else {
+  setupParentFrameMessageListener();
+  setupParentFrameDOM();
+}
